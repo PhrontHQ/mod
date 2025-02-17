798adcc32cdec78f6bd6ac374482c06fdadcf377
Thibault Zanini <thibaultzanini@gmail.com>
2025-02-17 14:49:42 +0100

    migrated teach button to ES6

--- ui/overlay.mod/teach/ui/main.mod/main.js
+++ ui/overlay.mod/teach/ui/main.mod/main.js
@@ -1,46 +1,35 @@
-var Component = require("mod/ui/component").Component;
+const { Component } = require("mod/ui/component");
 
-exports.Main = Component.specialize({
-    handleOpenOverlayButtonAction: {
-        value: function (event) {
-            this.overlay.show();
-        }
-    },
+exports.Main = class Main extends Component {
+    _overlay = undefined;
+
+    get overlay() {
+        return this._overlay;
+    }
+
+    set overlay(value) {
+        window.consentmentForm = this;
+        window.overlay = value;
+        this._overlay = value;
+    }
+
+    handleOpenOverlayButtonAction(event) {
+        this.overlay.show();
+    }
 
     // FIXME: When the overlay is displayed, it is attached to the root component,
     // placing it on a different node than the main component. As a result, the
     // close button's action event is not propagated to the main component,
     // preventing it from handling the event as intended.
-    handleCloseOverlayButtonAction: {
-        value: function () {
-            this.overlay.hide();
-        }
-    },
-
-    didShowOverlay: {
-        value: function (overlay) {
-            console.log("didShowOverlay", overlay);
-        }
-    },
-
-    didHideOverlay: {
-        value: function (overlay) {
-            console.log("didHideOverlay", overlay);
-        }
-    },
-
-    _overlay: {
-        value: undefined
-    },
-
-    overlay: {
-        get: function () {
-            return this._overlay;
-        },
-        set: function (value) {
-            window.consentmentForm = this;
-            window.overlay = value;
-            this._overlay = value;
-        }
+    handleCloseOverlayButtonAction() {
+        this.overlay.hide();
     }
-});
+
+    didShowOverlay(overlay) {
+        console.log("didShowOverlay", overlay);
+    }
+
+    didHideOverlay(overlay) {
+        console.log("didHideOverlay", overlay);
+    }
+};
