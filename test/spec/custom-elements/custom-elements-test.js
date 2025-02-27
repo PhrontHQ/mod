8c8df8698a728248b25bb1b77af7fb3c852c038e
Thibault Zanini <thibaultzanini@gmail.com>
2025-02-27 15:29:05 +0100

    fixed teach apps support new buttons

--- test/spec/custom-elements/custom-elements-test.js
+++ test/spec/custom-elements/custom-elements-test.js
@@ -5,7 +5,6 @@ var Montage = require("mod/core/core").Montage;
 var Component = require("mod/ui/component").Component;
 var Application = require("mod/core/application").application;
 // var MontageText = require("mod/ui/text.mod").Text;
-var MyButton = require("spec/custom-elements/my-button.mod").MyButton;
 
 var CustomElementTest = exports.CustomElementTest = Montage.specialize({
     textLabel2: {
