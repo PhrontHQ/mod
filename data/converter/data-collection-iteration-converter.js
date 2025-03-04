e828d321a6adeae9bca8b2b620fb4da9d6e6c3b4
Benoit Marchant <marchant@mac.com>
2025-03-05 00:25:32 +0100

    add DataCollectionIterationConverter

---
+++ data/converter/data-collection-iteration-converter.js
@@ -0,0 +1,42 @@
+/**
+ * @module mod/core/converter/collection-iteration-converter
+ * @requires mod/core/converter/converter
+ */
+var CollectionIterationConverter = require("core/converter/collection-iteration-converter").CollectionIterationConverter,
+    Montage = require("core/core").Montage;
+
+/**
+ * @class DataCollectionIterationConverter
+ * @classdesc Converts key/value arrays or an array of pairs to a Map.
+ * @extends Converter
+ */
+exports.DataCollectionIterationConverter = class DataCollectionIterationConverter extends CollectionIterationConverter {/** @lends DataCollectionIterationConverter */
+   
+    static {
+
+        Montage.defineProperties(this.prototype, {
+            _currentRule: {value: undefined}
+        });
+
+    }
+
+    constructor() {
+        super();
+    }
+
+    get currentRule() {
+        return this._currentRule;
+    }
+
+    set currentRule(value) {
+        if(value !== this._currentRule) {
+            this._currentRule = value;
+
+            //Pass down
+            this._convert.currentRule = value;
+            this._revert.currentRule = value;
+        }
+    }
+}
+
+
