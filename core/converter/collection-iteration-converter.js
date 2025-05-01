35788bbc3310393271c0053f4c58b84c33d7becd
Benoit Marchant <marchant@mac.com>
2025-05-01 14:02:02 -0700

    Fix a typo creating a bug

--- core/converter/collection-iteration-converter.js
+++ core/converter/collection-iteration-converter.js
@@ -210,7 +210,7 @@ exports.CollectionIterationConverter = Converter.specialize( /** @lends Collecti
                 if(Promise.is(iConvertedValue)) {
                     (promises || (promises = [])).push(iConvertedValue);
                 } else {
-                    (result || (result = new value.constructor)).add(iConverctedValue);
+                    (result || (result = new value.constructor)).add(iConvertedValue);
                 }
         
                 index++;
