76fabe62b2da07ff4c5a9717fe658ce0b58924e6
Thibault Zanini <thibaultzanini@gmail.com>
2025-07-18 17:10:42 +0200

    feat: migrate Image to ES6

--- ui/image.mod/image.js
+++ ui/image.mod/image.js
@@ -1,48 +1,48 @@
 /**
-    @module "mod/ui/native/image.mod"
-    @requires mod/ui/component
-    @requires mod/ui/native-control
-*/
-var Component = require("ui/component").Component;
+ * @module "mod/ui/native/image.mod"
+ * @requires mod/ui/component
+ * @requires mod/ui/native-control
+ */
+const { Component } = require("ui/component");
 
 /**
  * Wraps the a &lt;img> element with binding support for its standard attributes.
-   @class module:"mod/ui/native/image.mod".Image
-   @extends module:mod/ui/control.Control
+ * @class module:"mod/ui/native/image.mod".Image
+ * @extends module:mod/ui/control.Control
  */
-exports.Image = Component.specialize({
-    hasTemplate: {value: true }
+const Image = class Image extends Component {
+    hasTemplate = true;
+};
+
+/** @lends module:"mod/ui/native/image.mod".Image */
+Image.addAttributes({
+    /**
+     * A text description to display in place of the image.
+     * @type {string}
+     * @default null
+     */
+    alt: null,
+
+    /**
+     * The height of the image in CSS pixels.
+     * @type {number}
+     * @default null
+     */
+    height: null,
+
+    /**
+     * The URL where the image is located.
+     * @type {string}
+     * @default null
+     */
+    src: null,
+
+    /**
+     * The width of the image in CSS pixels.
+     * @type {number}
+     * @default null
+     */
+    width: null,
 });
 
-exports.Image.addAttributes(/** @lends module:"mod/ui/native/image.mod".Image */{
-
-/**
-    A text description to display in place of the image.
-    @type {string}
-    @default null
-*/
-        alt: null,
-
-/**
-    The height of the image in CSS pixels.
-    @type {number}
-    @default null
-*/
-        height: null,
-
-/**
-    The URL where the image is located.
-    @type {string}
-    @default null
-*/
-        src: null,
-
-/**
-    The width of the image in CSS pixels.
-    @type {number}
-    @default null
-*/
-        width: null
-
-
-});
+exports.Image = Image;
