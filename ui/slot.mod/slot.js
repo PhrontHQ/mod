772c60b1eb9d8def8cc0c561ae67adebbcbe264f
Thibault Zanini <thibaultzanini@gmail.com>
2025-07-24 15:55:06 +0200

    refactor: migrate Slot class to ES6  and improve delegate method handling

--- ui/slot.mod/slot.js
+++ ui/slot.mod/slot.js
@@ -1,8 +1,8 @@
 /**
-	@module "mod/ui/slot.mod"
-    @requires mod/ui/component
-*/
-var Component = require("../component").Component;
+ * @module "mod/ui/slot.mod"
+ * @requires mod/ui/component
+ */
+const { Component } = require("../component");
 
 /**
  * @class Slot
@@ -10,13 +10,7 @@ var Component = require("../component").Component;
  * other component.
  * @extends Component
  */
-exports.Slot = Component.specialize( /** @lends Slot.prototype # */ {
-
-    hasTemplate: {
-        enumerable: false,
-        value: false
-    },
-
+exports.Slot = class Slot extends Component {
     /**
      * An optional helper object.  The slot consults
      * `delegate.slotElementForComponent(component):Element` if available for
@@ -28,90 +22,74 @@ exports.Slot = Component.specialize( /** @lends Slot.prototype # */ {
      * @type {?Object}
      * @default null
      */
-    delegate: {
-        value: null
-    },
+    delegate = null;
 
-    _content: {
-        value: null
-    },
+    _content = null;
 
-    enterDocument:{
-        value:function (firstTime) {
-            if (firstTime) {
-                this.element.classList.add("slot-mod");
-            }
-
-            this.addEventListener("firstDraw", this, false);
-        }
-    },
-
-    exitDocument: {
-        value: function () {
-            this.removeEventListener("firstDraw", this, false);
-        }
-    },
-
-    handleFirstDraw: {
-        value: function () {
-            if (this.delegate && typeof this.delegate.slotContentDidFirstDraw === "function") {
-                this.delegate.slotContentDidFirstDraw(this);
-            }
-        }
-    },
+    get hasTemplate() {
+        return false;
+    }
 
     /**
      * The component that resides in this slot and in its place in the
      * template.
      * @type {Component}
      * @default null
-    */
-    content: {
-        get: function () {
-            return this._content;
-        },
-        set: function (value) {
-            var element,
-                content;
+     */
+    get content() {
+        return this._content;
+    }
 
-            if (value && typeof value.needsDraw !== "undefined") {
-                content = this._content;
+    set content(value) {
+        let element;
 
-                // If the incoming content was a component; make sure it has an element before we say it needs to draw
-                if (!value.element) {
-                    element = document.createElement("div");
+        if (value && typeof value.needsDraw !== "undefined") {
+            // If the incoming content was a component;
+            // make sure it has an element before we say it needs to draw
+            if (!value.element) {
+                element = document.createElement("div");
 
-                    if (this.delegate && typeof this.delegate.slotElementForComponent === "function") {
-                        element = this.delegate.slotElementForComponent(this, value, element);
-                    }
-                    value.element = element;
-                } else {
-                    element = value.element;
+                if (this.respondsToDelegateMethod("slotElementForComponent")) {
+                    element = this.callDelegateMethod("slotElementForComponent", this, value, element);
                 }
-
-                // The child component will need to draw; this may trigger a draw for the slot itself
-                this.domContent = element;
-                value.needsDraw = true;
-
+                value.element = element;
             } else {
-                this.domContent = value;
+                element = value.element;
             }
 
-            this._content = value;
-            this.needsDraw = true;
+            // The child component will need to draw;
+            // this may trigger a draw for the slot itself
+            this.domContent = element;
+            value.needsDraw = true;
+        } else {
+            this.domContent = value;
         }
-    },
+
+        this._content = value;
+        this.needsDraw = true;
+    }
+
+    enterDocument(firstTime) {
+        if (firstTime) {
+            this.element.classList.add("slot-mod");
+        }
+
+        this.addEventListener("firstDraw", this, false);
+    }
+
+    exitDocument() {
+        this.removeEventListener("firstDraw", this, false);
+    }
+
+    handleFirstDraw() {
+        this.callDelegateMethod("slotContentDidFirstDraw", this);
+    }
 
     /**
      * Informs the `delegate` that `slotDidSwitchContent(slot)`
      * @function
      */
-    contentDidChange: {
-        value: function () {
-            if (this.delegate && typeof this.delegate.slotDidSwitchContent === "function") {
-                this.delegate.slotDidSwitchContent(this);
-            }
-        }
+    contentDidChange() {
+        this.callDelegateMethod("slotDidSwitchContent", this);
     }
-
-});
+};
