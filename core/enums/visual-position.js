6f113fbe19eaf4433651bff618f142489ebeab64
Thibault Zanini <thibaultzanini@gmail.com>
2025-08-14 16:19:42 +0200

    refactor: VisualOrientation and VisualPosition enums

--- core/enums/visual-position.js
+++ core/enums/visual-position.js
@@ -1,9 +1,31 @@
-const { Enum } = require("../enum");
+const Enum = require("../enum").Enum;
+
+const positions = ["start", "end"];
+const classNames = positions.map((position) => `mod--position-${position}`);
 
 /**
+ * Visual position types for components.
  * @typedef {"start"|"end"} VisualPosition
  */
+
+/**
+ * VisualPosition enum with available position values.
+ * @type {Object.<string, VisualPosition>}
+ * @property {string} start - Position at the beginning/start
+ * @property {string} end - Position at the end
+ */
 exports.VisualPosition = new Enum().initWithMembersAndValues(
-    ["start", "end"],
-    ["mod--start", "mod--end"]
+    positions,
+    positions
+);
+
+/**
+ * CSS class names corresponding to visual positions.
+ * @type {Object.<string, string>}
+ * @property {string} start - CSS class "mod--position-start"
+ * @property {string} end - CSS class "mod--position-end"
+ */
+exports.VisualPositionClassNames = new Enum().initWithMembersAndValues(
+    positions,
+    classNames
 );
