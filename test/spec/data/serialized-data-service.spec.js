25bdaeb018e60fa90f2a6dd054be0af2030eb583
Thibault Zanini <thibaultzanini@gmail.com>
2025-10-28 16:03:52 +0100

    chore: setup SerializedDataService specs

---
+++ test/spec/data/serialized-data-service.spec.js
@@ -0,0 +1,8 @@
+const SerializedDataService =
+    require("mod/data/service/serialized-data-service.mod/serialized-data-service").SerializedDataService;
+
+describe("SerializedDataService", () => {
+    it("should handle read operations", () => {
+        // Test implementation goes here
+    });
+});
