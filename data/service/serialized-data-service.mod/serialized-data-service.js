f647d573af17d92ceb4f408a76dbb22eb5793856
Thibault Zanini <thibaultzanini@gmail.com>
2025-11-03 12:04:05 +0100

    feat: First version of the SerializedDataService

--- data/service/serialized-data-service.mod/serialized-data-service.js
+++ data/service/serialized-data-service.mod/serialized-data-service.js
@@ -1,97 +1,75 @@
-const fs = require('fs'),
-    path = require("path"),
-    RawDataService = require("../raw-data-service").RawDataService,
-    DataOperation = require("../data-operation").DataOperation;
+const RawDataService = require("../raw-data-service").RawDataService;
 
 /**
-*
-* @class SerializedDataService
-* @extends RawDataService
-* 
-* 
-* 
-* Example of mergeDataObjects can be found here
-* 
-* 
-* 
-* 
-* 
-*/
-const SerializedDataService = exports.SerializedDataService = class SerializedDataService extends RawDataService {/** @lends SerializedDataService */
+ * @class SerializedDataService
+ * @extends RawDataService
+ */
+exports.SerializedDataService = class SerializedDataService extends RawDataService {
     constructor() {
         super();
-
-        return this;
-
+        this.map = new Map();
     }
 
+    // FIXME: this method is temporary, for testing purposes only
+    registerInstancesForType(dataType, { location, require }) {
+        if (!location || !require) {
+            throw new Error("Both location and require must be provided");
+        }
+
+        this.map.set(dataType, {
+            location,
+            require,
+        });
+    }
 
     handleReadOperation(readOperation) {
-        let readOperationCompletionPromise;
+        // TODO: Temporary workaround — until RawDataService can lazily subscribe to incoming
+        // data operations, verify here whether this service should handle the operation.
+        if (!this.handlesType(readOperation.target)) return;
 
-        /*
-            This gives a chance to a delegate to do something async by returning a Promise from rawDataServiceWillHandleReadOperation(readOperation).
-            When that promise resolves, then we check if readOperation.defaultPrevented, if yes, the we don't handle it, otherwise we proceed.
+        const { location, require } = this.map.get(readOperation.target);
 
-            Wonky, WIP: needs to work without a delegate actually implementing it.
-            And a RawDataService shouldn't know about all that boilerplate
+        if (!location || !require) {
+            throw new Error(`No location or require registered for type ${readOperation.target.name}`);
+        }
 
-            Note: If there was a default delegate shared that would implement rawDataServiceWillHandleReadOperation by returning Promise.resolve(readOperation)
-            it might be simpler, but probably a bit less efficient
-
-        */
-        readOperationCompletionPromise = this.callDelegateMethod("rawDataServiceWillHandleReadOperation", this, readOperation);
-        if(readOperationCompletionPromise) {
-            readOperationCompletionPromise = readOperationCompletionPromise.then((readOperation) => {
-                if(!readOperation.defaultPrevented) {
-                    this._handleReadOperation(readOperation);
+        return require
+            .async(location)
+            .then((module) => {
+                if (!module || !module.montageObject) {
+                    throw new Error("Module not found or invalid module format: " + location);
                 }
+
+                let { montageObject: rawData } = module;
+                const { criteria } = readOperation;
+
+                if (criteria) {
+                    rawData = module.montageObject.filter(criteria.predicateFunction);
+                }
+
+                return this._finalizeHandleReadOperation(readOperation, rawData);
+            })
+            .catch((error) => {
+                console.error("Error loading serialized data:", error);
+                throw error;
             });
-        } else {
-            this._handleReadOperation(readOperation);
-        }
-
-        //If we've been asked to return a promise for the read Completion Operation, we do so. Again, this is fragile. IT HAS TO MOVE UP TO RAW DATA SERVICE
-        //WE CAN'T RELY ON INDIVIDUAL DATA SERVICE IMPLEMENTORS TO KNOW ABOUT THAT...
-        if(this.promisesReadCompletionOperation) {
-            return readOperationCompletionPromise;
-        }
-
     }
 
-    _handleReadOperation(readOperation) {
+    _finalizeHandleReadOperation(readOperation, rawData, readOperationCompletionPromiseResolve) {
+        const responseOperation = this.responseOperationForReadOperation(
+            readOperation.referrer ? readOperation.referrer : readOperation,
+            null,
+            rawData
+        );
 
-        /*
-            Until we solve more efficiently (lazily) how RawDataServices listen for and receive data operations, we have to check wether we're the one to deal with this:
-        */
-        if(!this.handlesType(readOperation.target)) { 
-            return;
-        }
-
-        let readOperationCompletionPromiseResolvers,
-            readOperationCompletionPromise, readOperationCompletionPromiseResolve, readOperationCompletionPromiseReject;
-
-
-        if(this.promisesReadCompletionOperation) {
-            readOperationCompletionPromiseResolvers = Promise.withResolvers();
-            readOperationCompletionPromise = readOperationCompletionPromiseResolvers.promise;
-            readOperationCompletionPromiseResolve = readOperationCompletionPromiseResolvers.resolve;
-            readOperationCompletionPromiseReject = readOperationCompletionPromiseResolvers.reject;
-        } else {
-            readOperationCompletionPromise = readOperationCompletionPromiseResolve = readOperationCompletionPromiseReject = undefined;
-        }
-
-        let rawData;
-
-        let responseOperation = this.responseOperationForReadOperation(readOperation.referrer ? readOperation.referrer : readOperation, null, rawData);
+        // @benoit:
+        // 1. why is this needed?
+        // 2. what is target here?
         responseOperation.target.dispatchEvent(responseOperation);
 
-        //Resolve once dispatchEvent() is completed, including any pending progagationPromise.
+        // Resolve once dispatchEvent() is completed, including any pending progagationPromise.
         responseOperation.propagationPromise.then(() => {
             readOperationCompletionPromiseResolve?.(responseOperation);
         });
-
-
     }
-
-}
+};
