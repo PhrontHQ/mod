7fcea876757a3ab877e650f682b928f8ea5583ac
Thibault Zanini <thibaultzanini@gmail.com>
2026-01-30 17:21:49 +0100

    feat: add responsive app example

---
+++ examples/responsive-app/ui/footer.mod/footer.js
@@ -0,0 +1,10 @@
+/**
+ * @module "ui/footer.mod"
+ */
+const Component = require("mod/ui/component").Component;
+
+/**
+ * @class Footer
+ * @extends Component
+ */
+exports.Footer = class Footer extends Component {};
---
+++ examples/responsive-app/ui/list-item.mod/list-item.js
@@ -0,0 +1,10 @@
+/**
+ * @module "ui/list-item.mod"
+ */
+const Component = require("mod/ui/component").Component;
+
+/**
+ * @class ListItem
+ * @extends Component
+ */
+exports.ListItem = class ListItem extends Component {};
