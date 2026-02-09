c4f5a80b365eabc0f997a9903fbb4666f8d41a06
maxwell-jordan <maxhipp@gmail.com>
2026-02-09 09:58:57 -0800

    reformat converter

--- core/converter/date-range-to-string-converter.js
+++ core/converter/date-range-to-string-converter.js
@@ -10,19 +10,48 @@ var Converter = require("./converter").Converter;
 var DateRangeToStringConverter = exports.DateRangeToStringConverter = Converter.specialize({
  
     /**
+     * Constructor for DateRangeToStringConverter.
+     * 
+     * @function
+     * @param {string|string[]} [locales] - A string with a BCP 47 language tag, or an array of such strings
+     * @param {Object} [options] - An object with formatting options for Intl.DateTimeFormat
+     */
+    constructor: {
+        value: function DateRangeToStringConverter(locales, options) {
+            this.locales = locales;
+            this.options = options;
+        }
+    },
+
+    /**
+     * The locales for date formatting.
+     * @type {string|string[]}
+     */
+    locales: {
+        value: null,
+        writable: true
+    },
+
+    /**
+     * The options for date formatting.
+     * @type {Object}
+     */
+    options: {
+        value: null,
+        writable: true
+    },
+
+    /**
      * Converts a date range to a formatted string representation.
      * 
      * @function
-     * @param {Date} startDate - The start date of the range
-     * @param {Date} endDate - The end date of the range
-     * @param {string|string[]} [locales] - A string with a BCP 47 language tag, or an array of such strings
-     * @param {Object} [options] - An object with formatting options for Intl.DateTimeFormat
+     * @param {Range<Date>} range - The date range to be formatted
      * @returns {string} The formatted date range string
      */
     convert: {
-        value: function (startDate, endDate, locales, options) {
-            const dateTimeFormat = new Intl.DateTimeFormat(locales, options);
-            return dateTimeFormat.formatRange(startDate, endDate);
+        value: function (range) {
+            const dateTimeFormat = new Intl.DateTimeFormat(this.locales, this.options);
+            return dateTimeFormat.formatRange(range.begin, range.end);
         }
     }
 });
