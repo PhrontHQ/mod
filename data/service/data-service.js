PhrontHQ/mod

commit f358ed9b4d1b71ebe87fc8cb9fdaa187a846bbf2

Fix issue saving array changes done in quick succession

data/service/data-service.js
@@ -4775,6 +4775,7 @@ DataService.addClassProperties(
                                     if (!isDataObjectBeingMapped) {
                                         shouldAdd = !addedValues[i] || !registeredAddedValues.has(addedValues[i]);
                                         registeredAddedValues.add(addedValues[i]);
+                                        manyChanges.addedValues.push(addedValues[i]);
                                     }
                                     if (shouldAdd) {
                                         self._addDataObjectPropertyDescriptorValueForInversePropertyDescriptor(

commit cf067f5967134d8073dae157fbc8edc48a30d290

Fix regression caused by f358ed9

data/service/data-service.js
@@ -4775,9 +4775,9 @@ DataService.addClassProperties(
                                     if (!isDataObjectBeingMapped) {
                                         shouldAdd = !addedValues[i] || !registeredAddedValues.has(addedValues[i]);
                                         registeredAddedValues.add(addedValues[i]);
-                                        manyChanges.addedValues.push(addedValues[i]);
                                     }
                                     if (shouldAdd) {
+                                        manyChanges.addedValues.push(addedValues[i])
                                         self._addDataObjectPropertyDescriptorValueForInversePropertyDescriptor(
                                             dataObject,
                                             propertyDescriptor,